230bf1613becdaaf2a880495219ff121e7d41dd8
thisisaaronland <thisisaaronland@localhost>
2026-06-13 12:37:19 -0700

    basically works except for the CORS part which totally does not work

--- app/follow_wasm/www/javascript/index.js
+++ app/follow_wasm/www/javascript/index.js
@@ -1,3 +1,160 @@
 window.addEventListener("load", function load(event){
 
+    const sources_el = document.querySelector("#sources");    
+    const voices_el = document.querySelector("#voices");
+    const messages_el = document.querySelector("#messages");
+
+    var working;
+    var voices;
+    
+    const list_voices = function(){
+
+	voices = window.speechSynthesis.getVoices();
+	const count_voices = voices.length;
+
+	if (count_voices == 0){
+	    setTimeout(list_voices, 200);
+	    return;
+	}
+	    
+	for (var i = 0; i < count_voices; i++) {
+	    
+	    const opt = document.createElement("option");
+	    opt.setAttribute("class", "voice");
+	    opt.setAttribute("value", i);
+	    
+	    if (voices[i].default){
+		opt.setAttribute("selected", "selected");
+	    }
+	    
+	    const label = voices[i].name + " (" + voices[i].lang + ")";
+	    opt.appendChild(document.createTextNode(label));
+	    
+	    voices_el.appendChild(opt);
+	}
+
+	voices_el.style.display = "block";
+    };
+    
+    const scrub = function(raw) {
+	const doc = new DOMParser().parseFromString(raw, 'text/html');
+	return doc.body.textContent || "";
+    };
+
+    const speak = function(text) {
+	
+	const utterance = new SpeechSynthesisUtterance(text);
+	
+	utterance.pitch = 1; 
+	utterance.rate = 1;
+
+	const v = parseInt(voices_el.value);
+	utterance.voice = voices[v]; 
+
+	window.speechSynthesis.speak(utterance);
+    };
+
+    const display = function(text){
+	console.log("Display", text);
+
+	const dt = new Date();
+	const ts = dt.toLocaleTimeString();
+
+	const time_el = document.createElement("span");
+	time_el.setAttribute("class", "timestamp");
+	time_el.appendChild(document.createTextNode(ts));
+
+	const msg_el = document.createElement("span");
+	msg_el.setAttribute("class", "message");
+	msg_el.appendChild(document.createTextNode(text));
+
+	const li_el = document.createElement("li");
+	li_el.appendChild(time_el);
+	li_el.appendChild(msg_el);
+
+	messages_el.prepend(li_el);	
+    };
+
+    const process = function(rsp){
+	
+	for (const raw of rsp.posts){
+	    const p = scrub(raw);
+	    speak(p);
+	    display(p);
+	}
+    };
+
+    const get_posts_from_text = function(uri, text){
+
+	working = true;
+	
+	get_posts(uri, text).then((rsp) => {
+	    
+	    try {
+		process(JSON.parse(rsp));
+	    } catch(err){
+		console.error("Failed to handle WASM response", err);
+	    }
+
+	    working = false;
+	    
+	}).catch((err) => {
+	    console.error("Failed to get posts from WASM", err);
+	    working = false;	    
+	});
+	
+    };
+    
+    const fetch_sources = function(){
+
+	if (working){
+	    return;
+	}
+
+	working = true;
+
+	if (sources.value == ""){
+	    working = false;
+	    return;
+	}
+	
+	const uris = sources.value.split("\n")
+	
+	if (uris.length == 0){
+	    working = false;
+	    return;
+	}
+
+	for (const uri of uris) {
+
+	    if (uri == "http://random.localhost"){
+		get_posts_from_text(uri, "");
+	    } else {
+
+		console.log("fetch", uri);
+		
+		fetch(uri).then((rsp) => {
+		    get_posts_from_text(uri, rsp);
+		}).catch((err) => {
+		    display(err);
+		    console.error("Failed to fetch", uri, err)
+		});
+	    }
+	}
+
+	working = false;
+    };
+    
+    list_voices();
+    
+    sfomuseum.golang.wasm.fetch("wasm/get-posts.wasm").then((rsp) => {
+
+	sources.removeAttribute("disabled");
+	setInterval(fetch_sources, 1000 * 10);
+	
+    }).catch((err) => {
+	console.error("Failed to load update get sources binary", err);
+        return;
+    });
+
 });
--- wasm/posts.go
+++ wasm/posts.go
@@ -4,6 +4,7 @@ package wasm
 import(
 	"context"
 	"net/url"
+	"log/slog"
 	"syscall/js"
 	"sync"
 	"fmt"
@@ -12,6 +13,11 @@ import(
 	"github.com/aaronland/go-liveblog/parser"	
 )
 
+type GetPostsResponse struct {
+	Posts []string `json:"posts"`
+	Errors []string `json:"errors"`
+}
+
 func GetPostsFunc() js.Func{
 
 	// Will this work?
@@ -19,66 +25,65 @@ func GetPostsFunc() js.Func{
 	
 	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 
-		count := len(args)
-		uris := make([]string, count)
-
-		for i := 0; i < count; i++ {
-			uris[i] = args[i].String()
-		}
-
+		source := args[0].String()
+		text := args[1].String()
+		
 		handler := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 
 			resolve := args[0]
 			reject := args[1]
 
 			ctx := context.Background()
+
 			all_posts := make([]string, 0)
 			
-			for _, uri := range uris {
-
-				// START OF put me in sync.Once
-				
-				u, err := url.Parse(uri)
-				
-				if err != nil {
-					reject.Invoke(fmt.Sprintf("Failed to parse URI, %w", err))
-					return nil
-				}
-				
-				p_uri := fmt.Sprintf("%s://", u.Host)
-				p, err := parser.NewParser(ctx, p_uri)
-
-				if err != nil {
-					reject.Invoke(fmt.Sprintf("Failed to create parser for %s, %w", p_uri, err))
-					return nil
-				}
-
-				// END OF put me in sync.Once
-
-				// START OF put me in parser.GetPostsWithCache
-				
-				_, posts, err := p.GetPosts(ctx, uri)
-
-				if err != nil {
-					reject.Invoke(fmt.Sprintf("Failed to derive posts for %s, %w", p_uri, err))
-					return nil
-				}
-
-				for _, p := range posts {
-					
-					_, exists := cache.LoadOrStore(p, true)
-					
-					if exists {
-						continue
-					}
-					
-					all_posts = append(all_posts, p)
-				}
-
-				// END OF put me in parser.GetPostsWithCache				
+			slog.Info("PARSE", "uri", source)
+			// START OF put me in sync.Once
+			
+			u, err := url.Parse(source)
+			
+			if err != nil {
+				reject.Invoke(fmt.Sprintf("Failed to parse URI, %w", err))
+				return nil
 			}
-
-			enc, err := json.Marshal(all_posts)
+			
+			p_uri := fmt.Sprintf("%s://", u.Host)
+			p, err := parser.NewParser(ctx, p_uri)
+			
+			if err != nil {
+				reject.Invoke(fmt.Sprintf("Failed to create parser for %s, %w", p_uri, err))
+				return nil
+			}
+			
+			// END OF put me in sync.Once
+			
+			// START OF put me in parser.GetPostsWithCache
+			
+			_, posts, err := p.GetPostsFromText(ctx, text)
+			
+			if err != nil {
+				reject.Invoke(fmt.Sprintf("Failed to derive posts for %s, %w", p_uri, err))
+				return nil
+			}
+			
+			for _, p := range posts {
+				
+				_, exists := cache.LoadOrStore(p, true)
+				
+				if exists {
+					continue
+				}
+				
+				all_posts = append(all_posts, p)
+			}
+			
+			// END OF put me in parser.GetPostsWithCache				
+			
+			rsp := GetPostsResponse{
+				Posts: all_posts,
+			}
+			
+			enc, err := json.Marshal(rsp)
 
 			if err != nil {
 				reject.Invoke(fmt.Sprintf("Failed to marshal posts, %w", err))
